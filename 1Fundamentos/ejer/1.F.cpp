a6241780b3dc458dddcc55f80668953aab68793c
pocotu <learning1830@outlook.com>
2024-12-24 00:47:48 -0500

    numberNegative

--- 1Fundamentos/ejer/1.F.cpp
+++ 1Fundamentos/ejer/1.F.cpp
@@ -1,2 +1,20 @@
 // Dado un número entero, determina si es positivo, 
 // negativo o cero
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x;
+    cout << "Enter an integer: ";
+    cin >> x;
+
+    if (x < 0) {
+        cout << "The number is negative." << endl;
+    } else if (x > 0) {
+        cout << "The number is positive." << endl;
+    } else {
+        cout << "The number is zero." << endl;
+    }
+}
