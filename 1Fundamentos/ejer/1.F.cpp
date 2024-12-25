c5b7be30b164cb82197f27b793ad0c88102ea178
pocotu <learning1830@outlook.com>
2024-12-25 12:16:20 -0500

    hoyo

--- 1Fundamentos/ejer/1.F.cpp
+++ 1Fundamentos/ejer/1.F.cpp
@@ -5,16 +5,3 @@
 
 using namespace std;
 
-int main() {
-    int x;
-    cout << "Enter an integer: ";
-    cin >> x;
-
-    if (x < 0) {
-        cout << "The number is negative." << endl;
-    } else if (x > 0) {
-        cout << "The number is positive." << endl;
-    } else {
-        cout << "The number is zero." << endl;
-    }
-}
